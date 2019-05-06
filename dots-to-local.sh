01aeb37fc8f88474225a5dcbf42f96d5cc0bf23c
David Mynors <dmynors@gmail.com>
2019-05-06 17:34:45 +0100

    remove fish config, revert to karabiner.json
    'yqrashawn/GokuRakuJoudo' wasn't cooperating with me

--- dots-to-local.sh
+++ dots-to-local.sh
@@ -3,14 +3,15 @@
 cd ~
 cp ~/GitHub/Active/dotfiles/.vimrc .
 
-cd ~/.tmux
-cp ~/GitHub/Active/dotfiles/.tmux/.tmux.conf .
-cp ~/GitHub/Active/dotfiles/.tmux/tmuxline .
-
 cd ~/.emacs.d
 cp ~/GitHub/Active/dotfiles/.emacs.d/init.el .
 
 cd ~/.config
-cp -R ~/GitHub/Active/dotfiles/.config/fish .
 cp -R ~/GitHub/Active/dotfiles/.config/nvim .
-cp ~/GitHub/Active/dotfiles/.config/karabiner.edn .
+
+cd ~/.config/karabiner
+cp ~/GitHub/Active/dotfiles/.config/karabiner/karabiner.json .
+
+cd ~/.tmux
+cp ~/GitHub/Active/dotfiles/.tmux/.tmux.conf .
+cp ~/GitHub/Active/dotfiles/.tmux/tmuxline .
--- local-to-dots.sh
+++ local-to-dots.sh
@@ -3,14 +3,15 @@
 cd ~/GitHub/Active/dotfiles
 cp ~/.vimrc .
 
-cd ~/GitHub/Active/dotfiles/.tmux
-cp ~/.tmux/.tmux.conf .
-cp ~/.tmux/tmuxline .
-
 cd ~/GitHub/Active/dotfiles/.emacs.d
 cp ~/.emacs.d/init.el .
 
 cd ~/GitHub/Active/dotfiles/.config
-cp -R ~/.config/fish .
 cp -R ~/.config/nvim .
-cp ~/.config/karabiner.edn .
+
+cd ~/GitHub/Active/dotfiles/.config/karabiner
+cp ~/.config/karabiner/karabiner.json .
+
+cd ~/GitHub/Active/dotfiles/.tmux
+cp ~/.tmux/.tmux.conf .
+cp ~/.tmux/tmuxline .
