2efd5ae52cab543685762c936558a77ad554c31d
David Mynors <dmynors@gmail.com>
2019-05-07 10:55:29 +0100

    remove custom-set-variables

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -171,17 +171,3 @@
 
 ;; Allow hash to be entered on UK macbook keyboard layout
 (global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (general use-package seeing-is-believing ruby-test-mode rbenv ranger projectile pdf-tools org-noter minitest interleave inf-ruby helm exec-path-from-shell evil-leader evil-escape eink-theme counsel))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
