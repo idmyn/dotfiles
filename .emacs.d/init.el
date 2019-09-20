e07c99f5cb668ecbefb68b83c61e971101cefbbb
idmyn <hello@davidmyno.rs>
2019-09-20 14:34:43 +0100

    bind command to run current file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -169,6 +169,81 @@
    (get-buffer-process (shell))
    "learn --f-f\n")))
 
+(defun xah-run-current-file ()
+  "Execute the current file.
+For example, if the current buffer is x.py, then it'll call 「python x.py」 in a shell.
+Output is printed to buffer “*xah-run output*”.
+
+The file can be Emacs Lisp, PHP, Perl, Python, Ruby, JavaScript, TypeScript, golang, Bash, Ocaml, Visual Basic, TeX, Java, Clojure.
+File suffix is used to determine what program to run.
+
+If the file is modified or not saved, save it automatically before run.
+
+URL `http://ergoemacs.org/emacs/elisp_run_current_file.html'
+Version 2018-10-12"
+  (interactive)
+  (let (
+        ($outputb "*xah-run output*")
+        (resize-mini-windows nil)
+        ($suffix-map
+         ;; (‹extension› . ‹shell program name›)
+         `(
+           ("php" . "php")
+           ("pl" . "perl")
+           ("py" . "python")
+           ("py3" . ,(if (string-equal system-type "windows-nt") "c:/Python32/python.exe" "python3"))
+           ("rb" . "ruby")
+           ("go" . "go run")
+           ("hs" . "runhaskell")
+           ("js" . "node")
+           ("mjs" . "node --experimental-modules ")
+           ("ts" . "tsc") ; TypeScript
+           ("tsx" . "tsc")
+           ("sh" . "bash")
+           ("clj" . "java -cp ~/apps/clojure-1.6.0/clojure-1.6.0.jar clojure.main")
+           ("rkt" . "racket")
+           ("ml" . "ocaml")
+           ("vbs" . "cscript")
+           ("tex" . "pdflatex")
+           ("latex" . "pdflatex")
+           ("java" . "javac")
+           ;; ("pov" . "/usr/local/bin/povray +R2 +A0.1 +J1.2 +Am2 +Q9 +H480 +W640")
+           ))
+        $fname
+        $fSuffix
+        $prog-name
+        $cmd-str)
+    (when (not (buffer-file-name)) (save-buffer))
+    (when (buffer-modified-p) (save-buffer))
+    (setq $fname (buffer-file-name))
+    (setq $fSuffix (file-name-extension $fname))
+    (setq $prog-name (cdr (assoc $fSuffix $suffix-map)))
+    (setq $cmd-str (concat $prog-name " \""   $fname "\" &"))
+    (run-hooks 'xah-run-current-file-before-hook)
+    (cond
+     ((string-equal $fSuffix "el")
+      (load $fname))
+     ((or (string-equal $fSuffix "ts") (string-equal $fSuffix "tsx"))
+      (if (fboundp 'xah-ts-compile-file)
+          (progn
+            (xah-ts-compile-file current-prefix-arg))
+        (if $prog-name
+            (progn
+              (message "Running")
+              (shell-command $cmd-str $outputb ))
+          (error "No recognized program file suffix for this file."))))
+     ((string-equal $fSuffix "go")
+      (xah-run-current-go-file))
+     ((string-equal $fSuffix "java")
+      (progn
+        (shell-command (format "java %s" (file-name-sans-extension (file-name-nondirectory $fname))) $outputb )))
+     (t (if $prog-name
+            (progn
+              (message "Running")
+              (shell-command $cmd-str $outputb ))
+          (error "No recognized program file suffix for this file."))))
+    (run-hooks 'xah-run-current-file-after-hook)))
+
 
 ;;; EDITOR NAVIGATION / INTERACTION
 
@@ -191,6 +266,8 @@
     "s--" 'text-scale-decrease
 
     "C-SPC" 'ivy-yasnippet)
+
+    (general-define-key (kbd "<C-return>") 'xah-run-current-file)
 
   ;; https://www.emacswiki.org/emacs/RecreateScratchBuffer
   (defun switch-to-scratch-and-back ()
@@ -221,8 +298,8 @@
     "h" 'highlight-indentation-mode
     "c" 'comment-or-uncomment-region-or-line
     "q" 'evil-quit
-    "v" 'split-window-right
-    "x" 'split-window-below
+    "v" (lambda () (interactive)(split-window-right) (other-window 1))
+    "x" (lambda () (interactive)(split-window-below) (other-window 1))
     "p" 'projectile-command-map
     "l" 'learn-tests
     "a" 'howdoyou-query
@@ -468,7 +545,8 @@
   "Set up `evil' bindings for `eshell'."
   (general-def 'insert eshell-mode-map
     "C-k" 'eshell-next-matching-input-from-input
-    "C-l" 'eshell-previous-matching-input-from-input))
+    "C-l" 'eshell-previous-matching-input-from-input
+    "C-;" 'eshell-send-input))
 (add-hook 'eshell-first-time-mode-hook 'eshell-setup-keys)
 
 (use-package load-bash-alias
