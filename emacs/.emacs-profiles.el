05a49933e43fb9135e0175a0b465304c24bbb9a8
idmyn <hello@davidmyno.rs>
2020-01-14 15:08:27 +0000

    add chemacs config

---
+++ emacs/.emacs-profiles.el
@@ -0,0 +1,3 @@
+(("default" . ((user-emacs-directory . "~/.emacs.d") (straight-p)))
+ ("spacemacs" . ((user-emacs-directory . "~/spacemacs")))
+ ("doom" . ((user-emacs-directory . "~/doom-emacs"))))
