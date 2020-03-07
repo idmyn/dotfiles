32c736d8e7814bdc55767076a26643bacae4e3ad
idmyn <hello@davidmyno.rs>
2020-03-07 20:47:47 +0000

    prevent newlines continuing comments, install doom

--- setup.sh
+++ setup.sh
@@ -17,3 +17,9 @@ if ! grep -Fxq "/usr/local/bin/fish" /etc/shells; then
   echo /usr/local/bin/fish | sudo tee -a /etc/shells
   chsh -s /usr/local/bin/fish
 fi
+
+# install doom emacs if it isn't installed already
+if [ ! -f "$HOME/.emacs.d/bin/doom" ]; then
+  git clone https://github.com/hlissner/doom-emacs "$HOME/.emacs.d"
+  "$HOME/.emacs.d/bin/doom" install
+fi
