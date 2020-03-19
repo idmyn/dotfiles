a5ef0d1869c8ea40103fa5d993a70924a92ebcfb
David Mynors <hello@davidmyno.rs>
2020-03-19 19:33:49 +0000

    move zsh/fish plugin install commands

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
-
-fish --command="fisher"
-fish --command="set -Ua fish_user_paths ~/.bin"
-
 # install doom emacs if it isn't installed already
 if [ ! -f "$HOME/.emacs.d/bin/doom" ]; then
   git clone https://github.com/hlissner/doom-emacs "$HOME/.emacs.d"
---
+++ shell/install-plugins.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
+
+fish --command="fisher"
+fish --command="set -Ua fish_user_paths ~/.bin"
