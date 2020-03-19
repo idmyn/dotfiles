33d306f634560d45312bebd308192abb1621329b
David Mynors <hello@davidmyno.rs>
2020-03-19 21:45:01 +0000

    add doom-emacs as a git submodule

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# install doom emacs if it isn't installed already
-if [ ! -f "$HOME/.emacs.d/bin/doom" ]; then
-  git clone https://github.com/hlissner/doom-emacs "$HOME/.emacs.d"
-  "$HOME/.emacs.d/bin/doom" install
-fi
