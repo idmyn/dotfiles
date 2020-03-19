fe397d2ac5ba7adc62179b9de86ab8d10f062f76
idmyn <hello@davidmyno.rs>
2020-03-19 18:39:26 +0000

    restore old vanilla emacs config for posterity

---
+++ emacs/vanilla/custom.el
@@ -0,0 +1,34 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(emojify-display-style (quote image))
+ '(emojify-download-emojis-p (quote ask))
+ '(emojify-emoji-set "twemoji-v2-22")
+ '(emojify-emoji-styles (quote (unicode)))
+ '(global-emojify-mode t)
+ '(global-hl-todo-mode t)
+ '(js2-mode-show-parse-errors nil)
+ '(projectile-globally-ignored-files (quote ("TAGS" ".DS_Store" ".learn" ".rspec" ".gitignore")))
+ '(show-paren-mode t)
+ '(smie-config (quote ((css-mode (2 :elem basic 4)))))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(deft-summary-face ((t nil)))
+ '(deft-title-face ((t (:inherit font-lock-function-name-face :weight bold))))
+ '(hl-line ((t (:background "lavender"))))
+ '(js2-error ((t nil)))
+ '(js2-warning ((t nil)))
+ '(line-number ((t (:inherit (shadow default) :family "Input Mono Narrow"))))
+ '(vterm-color-black ((t (:inherit term-color-black :background "dark gray"))))
+ '(web-mode-doctype-face ((t nil)))
+ '(web-mode-html-attr-name-face ((t nil)))
+ '(web-mode-html-tag-face ((t nil)))
+ '(web-mode-json-key-face ((t nil)))
+ '(web-mode-symbol-face ((t nil)))
+ '(whitespace-tab ((t (:foreground "#9e9e9e")))))
