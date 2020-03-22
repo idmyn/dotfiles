9272b44e2a4c9f4478aa064fa1578500fb4eaa77
David Mynors <hello@davidmyno.rs>
2020-03-22 10:30:51 +0000

    setup gcloud k8s with fish shell

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -16,3 +16,6 @@ if not functions -q fisher
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
+
+test -f ~/google-cloud-sdk/path.fish.inc && source ~/google-cloud-sdk/path.fish.inc
+test -f ~/google-cloud-sdk/completion.fish.inc && source ~/google-cloud-sdk/completion.fish.inc
