a9d710229e6acba24cff4962e94658c9c82756ba
David Mynors <hello@davidmyno.rs>
2020-04-01 08:29:21 +0100

    add finicky config

---
+++ finicky/.finicky.js
@@ -0,0 +1,10 @@
+module.exports = {
+  defaultBrowser: "Safari",
+  handlers: [
+    {
+      match: /.*/,
+      // Opens the first running browsers in the list. If none are running, the first one will be started.
+      browser: ["Safari", "Firefox", "Brave"]
+    }
+  ]
+};
