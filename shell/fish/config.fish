7c0e7b2aa83691a40312d947892606c20ffe2c63
David Mynors <hello@davidmyno.rs>
2020-05-02 13:21:42 +0100

    add vterm config

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -17,5 +17,21 @@ if not functions -q fisher
     fish -c fisher
 end
 
+# for emacs-libvterm
+function vterm_printf;
+    if [ -n "$TMUX" ]
+        # tell tmux to pass the escape sequences through
+        # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
+        printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+    else if string match -q -- "screen*" "$TERM"
+        # GNU screen (screen, screen-256color, screen-256color-bce)
+        printf "\eP\e]%s\007\e\\" "$argv"
+    else
+        printf "\e]%s\e\\" "$argv"
+    end
+end
+
 test -f ~/google-cloud-sdk/path.fish.inc && source ~/google-cloud-sdk/path.fish.inc
 test -f ~/google-cloud-sdk/completion.fish.inc && source ~/google-cloud-sdk/completion.fish.inc
+
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
