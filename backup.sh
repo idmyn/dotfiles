85e9d1f5033d0c09d6c05f14badbce00e34db2ec
David Mynors <hello@davidmyno.rs>
2020-09-27 13:18:49 +0100

    Update backup script exclusions, terminal colors

--- backup.sh
+++ backup.sh
@@ -11,20 +11,22 @@ echo "Beginning backup at $(date)" >> "$LOGFILE"
 
 . .backup.env
 
-/usr/local/bin/restic backup ~     \
-    --exclude="**/node_modules"    \
-    --exclude="${HOME}/desk"       \
-    --exclude="${HOME}/files"      \
-    --exclude="${HOME}/.asdf"      \
-    --exclude="${HOME}/.Trash"     \
-    --exclude="${HOME}/Library"    \
-    --exclude="${HOME}/Desktop"    \
-    --exclude="${HOME}/Documents"  \
-    --exclude="${HOME}/Pictures"   \
-    --exclude="${HOME}/Public"     \
-    --exclude="${HOME}/Movies"     \
-    --exclude="${HOME}/Music"      \
-    --exclude="${HOME}/Tresors"    \
+/usr/local/bin/restic backup ~       \
+    --exclude="**/node_modules"      \
+    --exclude="${HOME}/desk"         \
+    --exclude="${HOME}/files"        \
+    --exclude="${HOME}/.asdf"        \
+    --exclude="${HOME}/.emacs.d"     \
+    --exclude="${HOME}/.Trash"       \
+    --exclude="${HOME}/Library"      \
+    --exclude="${HOME}/Desktop"      \
+    --exclude="${HOME}/Documents"    \
+    --exclude="${HOME}/Applications" \
+    --exclude="${HOME}/Pictures"     \
+    --exclude="${HOME}/Public"       \
+    --exclude="${HOME}/Movies"       \
+    --exclude="${HOME}/Music"        \
+    --exclude="${HOME}/Tresors"      \
     >> "$LOGFILE" 2>&1
 
 printf "\n*******************************************************\n\n" >> "$LOGFILE"
--- shell/elvish/rc.elv
+++ shell/elvish/rc.elv
@@ -55,7 +55,7 @@ fn k { kubectl }
 fn b [@a]{ buku --suggest --colors aaeca --db $E:HOME"/.config/cloud/bookmarks.db" $@a }
 fn gs [@a]{ git status $@a }
 fn gb [@a]{ git branch $@a }
-fn ls [@a]{ echo; exa -F -I 'Desktop|Documents|Downloads|Library|Movies|Music|Pictures|Public|Tresors' $@a }
+fn ls [@a]{ echo; exa -F -I 'Applications|Desktop|Documents|Downloads|Library|Movies|Music|Pictures|Public|Tresors' $@a }
 fn la [@a]{ ls -a $@a }
 fn ll [@a]{ ls -alh $@a }
 fn cd [@a]{ cd $@a; la }
