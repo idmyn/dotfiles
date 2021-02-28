335699a8165384da4bcc8b40d08460ca6ceb9ad0
David Mynors <davidmy@nandos.co.uk>
2021-02-28 14:31:18 +0000

    Purge old config

--- offlineimap/password.py
+++
@@ -1,7 +0,0 @@
-# https://unix.stackexchange.com/a/48355
-import subprocess
-def password(acct):
-    try:
-        return subprocess.check_output(["pass", acct]).strip()
-    except subprocess.CalledProcessError:
-        return ""
