c3327b6d57e0eb1b58a8604ca93e728f5c96cc15
David Mynors <hello@davidmyno.rs>
2021-08-20 17:23:21 +0100

    Enable multiple-cursors, small nix fix

--- scripts/default.nix
+++ scripts/default.nix
@@ -14,14 +14,6 @@ let
   changed-files = scriptsFile "changed-files";
   backup = scriptsFile "backup";
 
-in [
-  e
-  eb
-  ks
-  jwt
-  note
-  podshell
-  kubesummary
-  changed-files
-  (if isWorkLaptop then null else backup)
-]
+  scripts = [ e eb ks jwt note podshell kubesummary changed-files ];
+
+in if isWorkLaptop then scripts else scripts ++ [ backup ]
