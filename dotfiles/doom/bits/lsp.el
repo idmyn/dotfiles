f603c7f0670f1f0fd1bce16df9149677a9ea8d82
David Mynors <hello@davidmyno.rs>
2022-05-11 18:31:39 +0100

    Minor tweaks

--- dotfiles/doom/bits/lsp.el
+++ dotfiles/doom/bits/lsp.el
@@ -9,16 +9,3 @@
   (map!
    :map lsp-ui-mode-map
    :n "g h" 'lsp-ui-doc-show))
-
-;; (defvar-local my/flycheck-local-cache nil)
-;;
-;; (defun my/flycheck-checker-get (fn checker property)
-;;   (or (alist-get property (alist-get checker my/flycheck-local-cache))
-;;       (funcall fn checker property)))
-;;
-;; (advice-add 'flycheck-checker-get :around 'my/flycheck-checker-get)
-;;
-;; (add-hook 'lsp-managed-mode-hook
-;;           (lambda ()
-;;             (when (derived-mode-p 'typescript-mode)
-;;               (setq my/flycheck-local-cache '((lsp . ((next-checkers . (javascript-eslint))))))))
