ce8ec55ab5f47e3ead188e50c2ea382e76a29a6d
David Mynors <hello@davidmyno.rs>
2023-09-02 10:43:24 +0100

    format with alejandra

--- flake.nix
+++ flake.nix
@@ -18,34 +18,37 @@
     };
   };
 
-
-  outputs = inputs@{ nixpkgs, nixpkgs-unstable, home-manager, darwin, ... }:
-    let
-      darwin-overlays = [
-        # Allow configurations to use pkgs.unstable.<package-name>.
-        (final: prev: {
-          unstable = import nixpkgs-unstable {
-            system = prev.system;
-            config.allowUnfree = true;
-          };
-        })
-      ];
-    in
-    {
-      darwinConfigurations = {
-        mbp = darwin.lib.darwinSystem {
-          system = "aarch64-darwin";
-          modules = [
-            { nixpkgs.overlays = darwin-overlays; }
-            ./darwin/configuration.nix
-            home-manager.darwinModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.david = import ./home.nix;
-            }
-          ];
+  outputs = inputs @ {
+    nixpkgs,
+    nixpkgs-unstable,
+    home-manager,
+    darwin,
+    ...
+  }: let
+    darwin-overlays = [
+      # Allow configurations to use pkgs.unstable.<package-name>.
+      (final: prev: {
+        unstable = import nixpkgs-unstable {
+          system = prev.system;
+          config.allowUnfree = true;
         };
+      })
+    ];
+  in {
+    darwinConfigurations = {
+      mbp = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          {nixpkgs.overlays = darwin-overlays;}
+          ./darwin/configuration.nix
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.david = import ./home.nix;
+          }
+        ];
       };
     };
+  };
 }
