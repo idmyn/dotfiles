63750674322b65e34251eabe01a4242164838692
David Mynors <hello@davidmyno.rs>
2024-03-18 19:17:14 +0000

    Update zed config

--- dotfiles/zed/themes/eink.ts
+++ dotfiles/zed/themes/eink.ts
@@ -1,13 +1,23 @@
 import solarized from "https://raw.githubusercontent.com/zed-industries/zed/main/assets/themes/solarized/solarized.json" with { type: "json" };
+import { colord } from "https://esm.sh/colord";
 
-const solarizedLight = solarized.themes.find(theme => theme.appearance === 'light')
+// maybe use Color.js for oklch support
+// actually maybe LCH in colord is fine
+// https://lch.oklch.com/
+
+const solarizedLight = solarized.themes.find((theme) =>
+  theme.appearance === "light"
+);
 
 const colors = {
-  bg: "#fffff8",
+  bg: "#FFFFF8",
   text: "#111111",
-  blue: "#278ad1ff",
   green: "#91EE92",
+  purple: "#484FD5",
+  ghostWhite: "#E1E7DF", // TODO improve
 };
+
+const darken = (color: string) => colord(color).darken(0.25).toHex();
 
 const theme = {
   $schema: "https://zed.dev/schema/themes/v0.1.0.json",
@@ -19,20 +29,24 @@ const theme = {
       appearance: "light",
       style: {
         ...solarizedLight.style,
-        // background: colors.bg,
-        // "toolbar.background": colors.bg,
-        // "editor.background": colors.bg,
-        // text: colors.text,
-        // "editor.gutter.background": colors.bg,
-        // "panel.background": colors.bg,
-        // "search.match_background": colors.green,
+        text: colors.text,
+        background: colors.bg,
+        "toolbar.background": colors.bg,
+        "editor.background": colors.bg,
+        "editor.gutter.background": colors.bg,
+        "panel.background": colors.bg,
+        "search.match_background": colors.green,
+        //"element.active": darken(colors.bg),
+        //"element.selected": darken(colors.bg),
+        "ghost_element.active": colors.ghostWhite,
+        "ghost_element.selected": colors.ghostWhite,
         syntax: {
           comment: {
             color: colors.text,
             font_weight: 700,
           },
           keyword: {
-            color: colors.blue,
+            color: colors.purple,
           },
         },
       },
@@ -40,4 +54,8 @@ const theme = {
   ],
 };
 
-Deno.writeTextFileSync("./eink.json", "// machine-generated from ./eink.ts\n// largely based on Zed's solarized theme\n" + JSON.stringify(theme, null, 2));
+Deno.writeTextFileSync(
+  "./eink.json",
+  "// machine-generated from ./eink.ts\n// largely based on Zed's solarized theme\n" +
+    JSON.stringify(theme, null, 2),
+);
